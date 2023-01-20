f17242b0dba8bd4de3495476677b3a942879ef05
Petr Reshetin <preshetin@gmail.com>
2023-01-20 11:35:19 +0500

    update to my name

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Peter Reshetin',
+    site_url: 'https://preshetin.com',
+    feed_url: 'https://preshetin.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
