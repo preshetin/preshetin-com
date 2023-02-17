a8555355290225a87ed8ae9367131b4f69b552cd
Petr Reshetin <preshetin@gmail.com>
2023-02-17 12:17:05 +0500

    remove pages since they are not shown

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -10,33 +10,33 @@ async function generate() {
     feed_url: 'https://preshetin.com/feed.xml'
   })
 
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
-  const allPosts = []
-  await Promise.all(
-    posts.map(async (name) => {
-      if (name.startsWith('index.')) return
+//   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
+//   const allPosts = []
+//   await Promise.all(
+//     posts.map(async (name) => {
+//       if (name.startsWith('index.')) return
 
-      const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
-      )
-      const frontmatter = matter(content)
+//       const content = await fs.readFile(
+//         path.join(__dirname, '..', 'pages', 'posts', name)
+//       )
+//       const frontmatter = matter(content)
 
-      allPosts.push({
-        title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
-        date: frontmatter.data.date,
-        description: frontmatter.data.description,
-        categories: frontmatter.data.tag.split(', '),
-        author: frontmatter.data.author
-      })
-    })
-  )
+//       allPosts.push({
+//         title: frontmatter.data.title,
+//         url: '/posts/' + name.replace(/\.mdx?/, ''),
+//         date: frontmatter.data.date,
+//         description: frontmatter.data.description,
+//         categories: frontmatter.data.tag.split(', '),
+//         author: frontmatter.data.author
+//       })
+//     })
+//   )
 
-  allPosts.sort((a, b) => new Date(b.date) - new Date(a.date))
-  allPosts.forEach((post) => {
-      feed.item(post)
-  })
-  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+//   allPosts.sort((a, b) => new Date(b.date) - new Date(a.date))
+//   allPosts.forEach((post) => {
+//       feed.item(post)
+//   })
+//   await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
 }
 
 generate()
